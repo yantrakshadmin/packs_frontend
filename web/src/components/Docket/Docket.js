06aa13397198346cbbae035543f8c30264d69d46
pcgamer1 <saxenasarthak72@gmail.com>
2020-06-30 22:51:18 +0530

    docket progress

--- web/src/components/Docket/Docket.js
+++ web/src/components/Docket/Docket.js
@@ -1,157 +1,230 @@
-import React from 'react';
-import {Row, Col, Typography, Table} from 'antd';
+import React, {useState} from 'react';
+import {Row, Col, Typography, Table, Spin} from 'antd';
 
 import './docket.styles.scss';
+import {useEffect} from 'react';
+import {retrieveAllotments} from 'common/api/auth';
 
 const {Title} = Typography;
 
-const Docket = () => {
-  return (
-    <div className="container">
-      <div className="header">
-        <div className="logo">
-          <img src={process.env.PUBLIC_URL + '/home-logo.png'} alt="Yantraksh" />
+const Docket = ({location}) => {
+  const [allotment, setAllotment] = useState(null);
+
+  useEffect(() => {
+    const fetchAllotment = async () => {
+      const {data} = await retrieveAllotments();
+      const reqData = data.filter((d) => d.id === location.state.id);
+      if (reqData) setAllotment(reqData[0]);
+      console.log(reqData[0]);
+    };
+    fetchAllotment();
+  }, [location]);
+
+  if (allotment)
+    return (
+      <div className="container">
+        <div className="header">
+          <div className="logo">
+            <img src={process.env.PUBLIC_URL + '/home-logo.png'} alt="Yantraksh" />
+          </div>
+          <div className="heading">
+            <Title level={2} style={{fontWeight: 'bold'}}>
+              DELIVERY CHALLAN
+            </Title>
+          </div>
         </div>
-        <div className="heading">
-          <Title level={2} style={{fontWeight: 'bold'}}>
-            DELIVERY CHALLAN
-          </Title>
-        </div>
+        <hr />
+        <Row className="meta">
+          <Col span={12} className="left">
+            <Row>
+              <Col span={10}>
+                <p style={{fontWeight: 'bold'}}>Transaction No. :</p>
+              </Col>
+              <Col span={12} style={{wordWrap: 'break-word'}}>
+                {allotment.transaction_no}
+              </Col>
+            </Row>
+            <Row>
+              <Col span={10}>
+                <p style={{fontWeight: 'bold'}}>Transaction Date :</p>
+              </Col>
+              <Col span={12} style={{wordWrap: 'break-word'}}>
+                10010101010
+              </Col>
+            </Row>
+            <Row>
+              <Col span={10}>
+                <p style={{fontWeight: 'bold'}}>Dispatch Date :</p>
+              </Col>
+              <Col span={12} style={{wordWrap: 'break-word'}}>
+                10010101010
+              </Col>
+            </Row>
+            <Row>
+              <Col span={10}>
+                <p style={{fontWeight: 'bold'}}>Transaction Type :</p>
+              </Col>
+              <Col span={12} style={{wordWrap: 'break-word'}}>
+                10010101010
+              </Col>
+            </Row>
+            <Row>
+              <Col span={4}>
+                <p style={{fontWeight: 'bold'}}>KIT ID :</p>
+              </Col>
+              <Col span={18} style={{wordWrap: 'break-word'}}>
+                10010101010
+              </Col>
+            </Row>
+          </Col>
+          <Col span={2}></Col>
+          <Col
+            span={10}
+            className="right"
+            style={{
+              fontFamily: 'Arial, Helvetica, sans-serif',
+            }}>
+            <p>
+              [ &nbsp;] Original for Consignee <br /> [ &nbsp;] Duplicate for Transporter <br />[
+              &nbsp;] Triplicate for Consignor
+            </p>
+          </Col>
+        </Row>
+        <Row className="main-data">
+          <Col span={12}>
+            <Row>
+              <Col span={10}>
+                <p style={{fontWeight: 'bold'}}>Sender's Name :</p>
+              </Col>
+              <Col span={12} style={{wordWrap: 'break-word'}}>
+                10010101010
+              </Col>
+            </Row>
+            <Row>
+              <Col span={10}>
+                <p style={{fontWeight: 'bold'}}>Sender's Address :</p>
+              </Col>
+              <Col span={12} style={{wordWrap: 'break-word'}}>
+                10010101010
+              </Col>
+            </Row>
+            <Row>
+              <Col span={10}>
+                <p style={{fontWeight: 'bold'}}>GST :</p>
+              </Col>
+              <Col span={12} style={{wordWrap: 'break-word'}}>
+                10010101010
+              </Col>
+            </Row>
+          </Col>
+
+          <Col span={12}>
+            <Row>
+              <Col span={10}>
+                <p style={{fontWeight: 'bold'}}>Receiver's Name :</p>
+              </Col>
+              <Col span={12} style={{wordWrap: 'break-word'}}>
+                10010101010
+              </Col>
+            </Row>
+            <Row>
+              <Col span={10}>
+                <p style={{fontWeight: 'bold'}}>Receiver's Address :</p>
+              </Col>
+              <Col span={12} style={{wordWrap: 'break-word'}}>
+                10010101010
+              </Col>
+            </Row>
+            <Row>
+              <Col span={10}>
+                <p style={{fontWeight: 'bold'}}>GST :</p>
+              </Col>
+              <Col span={12} style={{wordWrap: 'break-word'}}>
+                10010101010
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row className="table">
+          <Table />
+        </Row>
+        <Row className="final">
+          <Col span={12}>
+            <Row>
+              <Col span={8}>
+                <p style={{fontWeight: 'bold'}}>Amount in Words :</p>
+              </Col>
+              <Col span={14} style={{wordWrap: 'break-word'}}>
+                Thirty Thousand Four Hundred And Fifty Six Thousand Lakhs
+              </Col>
+            </Row>
+            <br />
+            <Row>
+              <Col span={8}>
+                <p style={{fontWeight: 'bold'}}>SO No. :</p>
+              </Col>
+              <Col span={14} style={{wordWrap: 'break-word'}}>
+                10010101010
+              </Col>
+            </Row>
+            <Row>
+              <Col span={8}>
+                <p style={{fontWeight: 'bold'}}>Transporter Name :</p>
+              </Col>
+              <Col span={14} style={{wordWrap: 'break-word'}}>
+                10010101010
+              </Col>
+            </Row>
+            <Row>
+              <Col span={8}>
+                <p style={{fontWeight: 'bold'}}>Driver Name :</p>
+              </Col>
+              <Col span={14} style={{wordWrap: 'break-word'}}>
+                _____________
+              </Col>
+            </Row>
+            <Row>
+              <Col span={8}>
+                <p style={{fontWeight: 'bold'}}>Driver No. : </p>
+              </Col>
+              <Col span={14} style={{wordWrap: 'break-word'}}>
+                _____________
+              </Col>
+            </Row>
+          </Col>
+
+          <Col span={12}>
+            <Row>
+              <Col span={8}>
+                <p style={{fontWeight: 'bold'}}>Grand Total :</p>
+              </Col>
+              <Col span={14} style={{wordWrap: 'break-word'}}>
+                10010101010
+              </Col>
+            </Row>
+            <Row>
+              <Col span={8}>
+                <p style={{fontWeight: 'bold'}}>Creation Date :</p>
+              </Col>
+              <Col span={14} style={{wordWrap: 'break-word'}}>
+                10010101010
+              </Col>
+            </Row>
+            <br /> <br />
+            <Row>
+              <Col span={8}>
+                <p style={{fontWeight: 'bold'}}>Vehicle No. :</p>
+              </Col>
+              <Col span={14} style={{wordWrap: 'break-word'}}>
+                10010101010
+              </Col>
+            </Row>
+          </Col>
+        </Row>
       </div>
-      <hr />
-      <Row className="meta">
-        <Col span={12} className="left">
-          <Row>
-            <Title level={4} style={{fontWeight: 'bold'}}>
-              Transaction No. :
-            </Title>
-          </Row>
-          <Row>
-            <Title level={4} style={{fontWeight: 'bold'}}>
-              Transaction Date :
-            </Title>
-          </Row>
-          <Row>
-            <Title level={4} style={{fontWeight: 'bold'}}>
-              Dispatch Date :
-            </Title>
-          </Row>
-          <Row>
-            <Title level={4} style={{fontWeight: 'bold'}}>
-              Transaction Type :
-            </Title>
-          </Row>
-          <Row>
-            <Title level={4} style={{fontWeight: 'bold'}}>
-              KIT ID :
-            </Title>
-          </Row>
-        </Col>
-        <Col
-          span={12}
-          className="right"
-          style={{
-            fontSize: '18px',
-            fontFamily: 'Arial, Helvetica, sans-serif',
-            // fontWeight: 'bold',
-          }}>
-          <p>
-            [ &nbsp;] Original for Consignee <br /> [ &nbsp;] Duplicate for Transporter <br />[
-            &nbsp;] Triplicate for Consignor
-          </p>
-        </Col>
-      </Row>
-      <Row className="main-data">
-        <Col span={12}>
-          <div className="left-main">
-            <Row>
-              <Title level={4} style={{fontWeight: 'bold'}}>
-                Sender's Name :
-              </Title>
-            </Row>
-            <Row>
-              <Title level={4} style={{fontWeight: 'bold'}}>
-                Sender's Address :
-              </Title>
-            </Row>
-            <Row>
-              <Title level={4} style={{fontWeight: 'bold'}}>
-                GST :
-              </Title>
-            </Row>
-          </div>
-        </Col>
-        <Col span={12}>
-          <div className="right-main">
-            <Row>
-              <Title level={4} style={{fontWeight: 'bold'}}>
-                Receiver's Name :
-              </Title>
-            </Row>
-            <Row>
-              <Title level={4} style={{fontWeight: 'bold'}}>
-                Receiver's Address :
-              </Title>
-            </Row>
-            <Row>
-              <Title level={4} style={{fontWeight: 'bold'}}>
-                GST :
-              </Title>
-            </Row>
-          </div>
-        </Col>
-      </Row>
-      <Row className="table">
-        <Table />
-      </Row>
-      <Row className="final">
-        <Col span={12}>
-          <Row>
-            <Title level={4} style={{fontWeight: 'bold'}}>
-              Amount in Words :
-            </Title>
-          </Row>
-          <Row>
-            <Title level={4} style={{fontWeight: 'bold'}}>
-              SO No. :
-            </Title>
-          </Row>
-          <Row>
-            <Title level={4} style={{fontWeight: 'bold'}}>
-              Transporter Name :
-            </Title>
-          </Row>
-          <Row>
-            <Title level={4} style={{fontWeight: 'bold'}}>
-              Driver Name : _____________
-            </Title>
-          </Row>
-          <Row>
-            <Title level={4} style={{fontWeight: 'bold'}}>
-              Driver No. : _____________
-            </Title>
-          </Row>
-        </Col>
-        <Col span={12}>
-          <Row>
-            <Title level={4} style={{fontWeight: 'bold'}}>
-              Grand Total :
-            </Title>
-          </Row>
-          <Row>
-            <Title level={4} style={{fontWeight: 'bold'}}>
-              Creation Date :
-            </Title>
-          </Row>
-          <br /> <br />
-          <Row>
-            <Title level={4} style={{fontWeight: 'bold'}}>
-              Vehicle No. :
-            </Title>
-          </Row>
-        </Col>
-      </Row>
-    </div>
+    );
+  return (
+    <Spin spinning={true} style={{position: 'absolute', marginLeft: '49vw', marginTop: '49vh'}} />
   );
 };
 
