032fe4d7c1724c1f8356779c8a67b9bf615b916f
Rahul Sharma <yantralogistics@gmail.com>
2020-07-17 21:17:50 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5a816498c17d98fb0efaba627c0b5517.js"
+  "/precache-manifest.2fd72722a0675f5aef65d6d59ebf56de.js"
 );
 
 self.addEventListener('message', (event) => {
