fa405e6f8c4e1620de25ade8965be4fb08fe04ac
pcgamer1 <saxenasarthak72@gmail.com>
2020-07-18 20:41:40 +0530

    fixes

--- web/src/components/Report.js
+++ web/src/components/Report.js
@@ -1,8 +1,69 @@
-import React from 'react';
-// import {Pivot} from 'webdatarocks';
+import React, {useState, useEffect} from 'react';
+import * as FlexmonsterReact from 'react-flexmonster/hooks';
+import {retrieveProducts} from 'common/api/auth';
+import storage from 'redux-persist/lib/storage';
+
+import 'flexmonster/flexmonster.css';
 
 const Report = () => {
-  return <Pivot report="https://cdn.webdatarocks.com/reports/report.json" />;
+  const [reqData, setReqData] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const {data} = await retrieveProducts();
+      console.log(typeof data);
+      if (data) {
+        setReqData(data);
+        console.log(data);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  return reqData ? (
+    // <WebDataRocksReact.Pivot
+    //   toolbar={true}
+    //   report={{
+    //     dataSource: {
+    //       data: reqData,
+    //     },
+    //   slice: {
+    //     rows: [
+    //       {
+    //         uniqueName: 'short_code',
+    //       },
+    //     ],
+    //     columns: [
+    //       {
+    //         uniqueName: 'priceperunit',
+    //       },
+    //     ],
+    //   },
+    // }}
+    <FlexmonsterReact.Pivot
+      // ref={ref}
+      toolbar={true}
+      width="100%"
+      report={{
+        dataSource: {
+          data: reqData,
+        },
+        slice: {
+          rows: [{uniqueName: 'short_code'}, {uniqueName: '[Measures]'}],
+          columns: [{uniqueName: 'Category'}],
+          measures: [
+            {
+              uniqueName: 'priceperunit',
+              aggregation: 'sum',
+            },
+          ],
+        },
+      }}
+      // reportcomplete={onReportComplete}
+    />
+  ) : // report="https://cdn.webdatarocks.com/reports/report.json"
+  // />
+  null;
 };
 
 export default Report;
