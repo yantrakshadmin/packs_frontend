05ee81b9722261980b918c6f7efeeaaa98c41523
pcgamer1 <saxenasarthak72@gmail.com>
2020-07-21 09:06:33 +0530

    fixes

--- web/src/icons/Download.js
+++ web/src/icons/Download.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 const Download = ({color}) => {
   return (
-    <i
-      className="fas fa-file-download"
-      style={{color: '#7CFC00', fontSize: 20, marginRight: '5px'}}></i>
+    <i className="fas fa-download" style={{color: '#7CFC00', fontSize: 20, marginRight: '5px'}}></i>
   );
 };
 
