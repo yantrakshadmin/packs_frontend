db1c76c11f9c4ca7b2673e94c4309353dfcfbe98
Sarthak Saxena <saxenasarthak72@gmail.com>
2020-08-19 19:06:07 +0530

    return rep fixes

--- web/src/components/Reports/Return.js
+++ web/src/components/Reports/Return.js
@@ -6,7 +6,7 @@ import {DEFAULT_BASE_URL} from 'common/constants/enviroment';
 import {useAPI} from 'common/hooks/api';
 import {Row, Col, Form, Button} from 'antd';
 import {FORM_ELEMENT_TYPES} from '../../constants/formFields.constant';
-import {retrieveReturnReport, retrieveClients} from 'common/api/auth';
+import {retrieveReturnReport} from 'common/api/auth';
 import returnColumns from 'common/columns/Return.column';
 import TableWithTabHoc from 'hocs/TableWithTab.hoc';
 import {useEffect} from 'react';
@@ -17,28 +17,12 @@ const AllotmentReport = ({currentPage}) => {
   const [csvData, setCsvData] = useState(null);
   const [reportData, setReportData] = useState(null);
   const [reqReturns, setReqAllotments] = useState(null);
-  const [client, setClient] = useState('');
-  const [clientName, setClientName] = useState(null);
   const [to, setTo] = useState(null);
   const [from, setFrom] = useState(null);
   const [form] = Form.useForm();
 
-  const {data: clients} = useAPI('/clients/', {});
-
   const onSubmit = async (data) => {
     setLoading(true);
-    if (!data['cname']) {
-      data['cname'] = '';
-      setClient(data['cname']);
-    } else {
-      setClient(data['cname']);
-      let reqC = null;
-      const {data: clients} = await retrieveClients();
-      clients.forEach((c) => {
-        if (c.user === data['cname']) reqC = c.client_name;
-      });
-      setClientName(reqC);
-    }
     data['to'] = moment(data['to']).format('YYYY-MM-DD HH:MM');
     data['from'] = moment(data['from']).format('YYYY-MM-DD HH:MM');
     setTo(data['to']);
@@ -114,24 +98,6 @@ const AllotmentReport = ({currentPage}) => {
     <>
       <Form onFinish={onSubmit} form={form} layout="vertical" hideRequiredMark autoComplete="off">
         <Row>
-          <Col span={10}>
-            {formItem({
-              key: 'cname',
-              kwargs: {
-                placeholder: 'Select',
-              },
-              others: {
-                selectOptions: clients || [],
-                key: 'user',
-                customTitle: 'client_name',
-                dataKeys: ['client_shipping_address'],
-              },
-              type: FORM_ELEMENT_TYPES.SELECT,
-              customLabel: 'Client',
-            })}
-          </Col>
-        </Row>
-        <Row>
           <Col span={3}>
             {formItem({
               key: 'from',
@@ -172,7 +138,7 @@ const AllotmentReport = ({currentPage}) => {
         size="middle"
         title="Return Dockets"
         hideRightButton
-        downloadLink={`${DEFAULT_BASE_URL}/return-reportsdownload/?cname=${client}&to=${to}&from=${from}`}
+        downloadLink={`${DEFAULT_BASE_URL}/return-reportsdownload/?to=${to}&from=${from}`}
         rowKey="id"
         // expandHandleKey="flows"
         // ExpandBody={AllotFlowTable}
