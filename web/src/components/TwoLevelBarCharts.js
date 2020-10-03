e20582a779d05b2afb4f0c6d8d08ed0b539f934e
alhacen <alhaqhassan@gmail.com>
2020-10-03 18:02:22 +0530

    added bar chart

--- web/src/components/TwoLevelBarCharts.js
+++ web/src/components/TwoLevelBarCharts.js
@@ -1,80 +1,56 @@
-/* eslint-disable react/no-multi-comp */
+// TwoLevelBarCharts| const { data,title } = this.props;
 import React, { PureComponent } from 'react';
 import {
-  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+  BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 } from 'recharts';
 
-// let data = [
+// const data = [
 //   {
-//     name: 'Page A', Returns: 4000, Allotments: 2400,
+//     name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
 //   },
 //   {
-//     name: 'Page B', Return: 3000, Allotments: 1398,
+//     name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
 //   },
 //   {
-//     name: 'Page C', Return: 2000, Allotments: 9800,
+//     name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
 //   },
 //   {
-//     name: 'Page D', Return: 2780, Allotments: 3908,
+//     name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
 //   },
 //   {
-//     name: 'Page E', Return: 1890, Allotments: 4800,
+//     name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
 //   },
 //   {
-//     name: 'Page F', Return: 2390, Allotments: 3800,
+//     name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
 //   },
 //   {
-//     name: 'Page G', Return: 3490, Allotments: 4300,
+//     name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
 //   },
 // ];
 
-class CustomizedLabel extends PureComponent {
-  render() {
-    const {
-      x, y, stroke, value,
-    } = this.props;
-
-    return <text x={x} y={y} dy={-4} fill={stroke} fontSize={10} textAnchor="middle">{value}</text>;
-  }
-}
-
-class CustomizedAxisTick extends PureComponent {
-  render() {
-    const {
-      x, y, stroke, payload,
-    } = this.props;
-
-    return (
-      <g transform={`translate(${x},${y})`}>
-        <text x={0} y={0} dy={16} textAnchor="end" fill="#666" transform="rotate(-35)">{payload.value}</text>
-      </g>
-    );
-  }
-}
-
 export default class TwoLevelBarCharts extends PureComponent {
-  static jsfiddleUrl = 'https://jsfiddle.net/alidingling/5br7g9d6/';
+  static jsfiddleUrl = 'https://jsfiddle.net/alidingling/30763kr7/';
 
   render() {
-    const { data,title } = this.props;
-console.log(data)
+    const { data,type } = this.props;
+
+    console.log(this.props)
     return (
-      <LineChart
-        width={800}
+      <BarChart
+        width={500}
         height={300}
         data={data}
         margin={{
-          top: 20, right: 30, left: 20, bottom: 10,
+          top: 5, right: 30, left: 20, bottom: 5,
         }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" height={60} tick={<CustomizedAxisTick />} />
+        <XAxis dataKey="name" />
         <YAxis />
         <Tooltip />
         <Legend />
-        <Line type="monotone" dataKey="Allotments" stroke="#8884d8" label={<CustomizedLabel />} />
-        <Line type="monotone" dataKey="Return" stroke="#82ca9d" />
-      </LineChart>
+        <Bar dataKey={type} fill="#8884d8" />
+      </BarChart>
     );
   }
 }
