6cc8ef64ab5d5b56a3a36f318a927295e47546c5
ashertoufeeq <ashertoufeeq@gmail.com>
2021-01-06 19:52:37 +0530

    Again debugging

--- common/helpers/mrHelper.js
+++ common/helpers/mrHelper.js
@@ -1,12 +1,15 @@
 export const mergeArray=(a,b)=>{
   return a.map((item) => {
     const obj = b.filter(i => i.mr === item.id)[0];
-    return  { ...item,
-      is_rejected:obj.is_rejected,
-      reason:obj.reason,
-      mr:obj.mr,
-      remark:obj.remark
+    if(obj){
+      return  { ...item,
+        is_rejected:obj.is_rejected,
+        reason:obj.reason,
+        mr:obj.mr,
+        remark:obj.remark
+      }
     }
+    return item
   });
 }
 
