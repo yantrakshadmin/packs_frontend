378a85c4c30ac358cd813961bc3ce8342f4ad75c
ashertoufeeq <ashertoufeeq@gmail.com>
2021-01-12 19:28:06 +0530

    After Writing Alot on CP

--- common/constants/enviroment.js
+++ common/constants/enviroment.js
@@ -1,4 +1,4 @@
 // export const DEFAULT_BASE_URL = 'https://packsdev.ap-south-1.elasticbeanstalk.com';
- export const DEFAULT_BASE_URL = 'https://packsb.yantraksh.com/';
-//export const DEFAULT_BASE_URL = 'http://127.0.0.1:8000';
+export const DEFAULT_BASE_URL = 'https://packsb.yantraksh.com/';
+// export const DEFAULT_BASE_URL = 'https://3f65d7f4aa1f.ngrok.io/';
 // export const DEFAULT_BASE_URL = 'http://samplebackend-env.eba-kwpeeyxu.ap-south-1.elasticbeanstalk.com/';
