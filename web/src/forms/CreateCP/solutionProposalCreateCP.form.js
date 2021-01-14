5446bbee141afeda7e94651efad0644bfdd93367
Rumeet Singh <singh.rumeet5@gmail.com>
2021-01-14 13:29:47 +0530

    Cp form autocomplete completed

--- web/src/forms/CreateCP/solutionProposalCreateCP.form.js
+++ web/src/forms/CreateCP/solutionProposalCreateCP.form.js
@@ -9,85 +9,124 @@ import {
 } from 'common/actions';
 import { solutionProposalCreateCPFormFields , }
   from 'common/formFields/createCP/solutionProposalCreateCP.formFields';
-import { getFields, getLabels,getFieldsByColumn } from 'common/constants/solutionproposalCreateCP';
+import { getFields, getLabels,getFieldsByColumn,getDefaultMonthValue } from 'common/constants/solutionproposalCreateCP';
+
+import _ from 'lodash';
 
 
 export const SolutionProposalCreateCPForm = ({ id, onCancel,lead,onNext,active }) => {
-  const [loading,setLoading] = useState(false);
-  const [form] = Form.useForm();
-  const dispatch = useDispatch();
-  const state =  useSelector(e=>(e.data.createCPData))
-  const [fields,setFields] = useState(getFields('FLC','Insert'))
-  const [labels,setLabels] = useState(getLabels('FLC','Insert'))
-  const submit = async (data) =>{
-    setLoading(true)
-    await dispatch({ type:ADD_CREATE_CP_DATA,
-      data:{ ...data } });
-    setLoading(false)
-    if(active === 1){
-      onNext();
-    }
-  }
-
-  useEffect( ()=>{
-    if(active!==1){
-      form.submit()
-      dispatch({ type:STOP_STEP_LOADING })
-    }
-  },[active])
-
-  useEffect( () => {
-    if (form.getFieldValue('kit_based_on_usage_ratio')) {
-      const totalKitQtysCols = getFieldsByColumn(form.getFieldValue('standard_assets'),form.getFieldValue('insert_type'),'quantity');
-      totalKitQtysCols.forEach(i => {
-        if (!form.getFieldValue(i)) {
-          if (i!=="mould_quantity") {
-            form.setFieldsValue({
-              [i] : form.getFieldValue('kit_based_on_usage_ratio'),
-            })
-          } else {
-            form.setFieldsValue({
-              ['mould_quantity'] : 1,
-            })
-          }
-        }
-      })
-    }
-  },[form])
-
-  const handleFieldsChange = useCallback(data => {
-
-    console.log(data[0].name[0]);
-
-	if (data[0].name[0]==="standard_assets" || data[0].name[0]==='insert_type') {
-		setFields(getFields(form.getFieldValue('standard_assets'),form.getFieldValue('insert_type')));
-		setLabels(getLabels(form.getFieldValue('standard_assets'),form.getFieldValue('insert_type')));
+	const [loading,setLoading] = useState(false);
+	const [form] = Form.useForm();
+	const dispatch = useDispatch();
+	const state =  useSelector(e=>(e.data.createCPData))
+	const [fields,setFields] = useState(getFields('FLC','Insert'))
+	const [labels,setLabels] = useState(getLabels('FLC','Insert'))
+	const submit = async (data) =>{
+		setLoading(true)
+		await dispatch({ type:ADD_CREATE_CP_DATA,
+			data:{ ...data } 
+		});
+		setLoading(false)
+		if(active === 1){
+			onNext();
+		}
 	}
 
+	useEffect( ()=>{
+		if(active!==1){
+		form.submit()
+		dispatch({ type:STOP_STEP_LOADING })
+		}
+	},[active])
 
-    const qtyPerKitCols = getFieldsByColumn(form.getFieldValue('standard_assets'),form.getFieldValue('insert_type'),'quantity_perkit');
-    const rateCols = getFieldsByColumn(form.getFieldValue('standard_assets'),form.getFieldValue('insert_type'),'rate');
-    const totalMatReqCols = getFieldsByColumn(form.getFieldValue('standard_assets'),form.getFieldValue('insert_type'),'tot_mat_req');
-    const totalCostCols = getFieldsByColumn(form.getFieldValue('standard_assets'),form.getFieldValue('insert_type'),'total_cost');
-
-    qtyPerKitCols.forEach((i,idx) => {
-		if (data[0].name[0]===qtyPerKitCols[idx] || data[0].name[0]===rateCols[idx]) {
-			if (form.getFieldValue(qtyPerKitCols[idx]) && form.getFieldValue(rateCols[idx])) {
-				const x = form.getFieldValue(qtyPerKitCols[idx])*form.getFieldValue(rateCols[idx]);
+	const updateTotalKitQtysCols = useCallback(() => {
+		if (form.getFieldValue('kit_based_on_usage_ratio')) {
+			const totalKitQtysCols = getFieldsByColumn(form.getFieldValue('standard_assets'),form.getFieldValue('insert_type'),'quantity');
+			totalKitQtysCols.forEach(i => {
+			if (!form.getFieldValue(i)) {
+				if (i!=="mould_quantity") {
 				form.setFieldsValue({
-					[totalMatReqCols[idx]] : x,
-					[totalCostCols[idx]] : Math.pow(x,2),
+					[i] : form.getFieldValue('kit_based_on_usage_ratio'),
 				})
-			} else {
+				} else {
 				form.setFieldsValue({
-					[totalMatReqCols[idx]] : 0,
-					[totalCostCols[idx]] : 0,
+					['mould_quantity'] : 1,
+				})
+				}
+			}
+			})
+		}
+	},[form])
+
+	const updateMonthCols = useCallback(() => {
+		const monthCols = getFieldsByColumn(form.getFieldValue('standard_assets'),form.getFieldValue('insert_type'),'month');
+		monthCols.forEach(i => {
+			if (!form.getFieldValue(i)) {
+				//console.log("HAHA: " + _.chain(i).trimEnd("month").trimEnd("_"))
+				form.setFieldsValue({
+					[i] : getDefaultMonthValue(i.slice(0,-6)),
 				})
 			}
-		}
-    })
+		})
+	},[form])
 
-  },[form,fields,labels])
+	useEffect( () => {
+		updateTotalKitQtysCols();
+		updateMonthCols();
+	},[])
+
+  	const handleFieldsChange = useCallback(data => {
+
+		if(data[0]){
+			if(data[0].name) {
+
+				//console.log(data[0].name[0]);
+
+				if (data[0].name[0]==="standard_assets" || data[0].name[0]==='insert_type') {
+					setFields(getFields(form.getFieldValue('standard_assets'),form.getFieldValue('insert_type')));
+					setLabels(getLabels(form.getFieldValue('standard_assets'),form.getFieldValue('insert_type')));
+					updateTotalKitQtysCols();
+					updateMonthCols();
+				}
+
+				const qtyPerKitCols = getFieldsByColumn(form.getFieldValue('standard_assets'),form.getFieldValue('insert_type'),'quantity_perkit');
+				const rateCols = getFieldsByColumn(form.getFieldValue('standard_assets'),form.getFieldValue('insert_type'),'rate');
+				const totalMatReqCols = getFieldsByColumn(form.getFieldValue('standard_assets'),form.getFieldValue('insert_type'),'tot_mat_req');
+				const totalCostCols = getFieldsByColumn(form.getFieldValue('standard_assets'),form.getFieldValue('insert_type'),'total_cost');
+				const monthCols = getFieldsByColumn(form.getFieldValue('standard_assets'),form.getFieldValue('insert_type'),'month');
+				const depCostCols = getFieldsByColumn(form.getFieldValue('standard_assets'),form.getFieldValue('insert_type'),'dep_cost');
+
+				qtyPerKitCols.forEach((i,idx) => {
+					if (data[0].name[0]===qtyPerKitCols[idx] || data[0].name[0]===rateCols[idx] || data[0].name[0]===totalMatReqCols[idx] || data[0].name[0]===totalCostCols[idx] || data[0].name[0]===depCostCols[idx]) {
+						if (form.getFieldValue(qtyPerKitCols[idx]) && form.getFieldValue(rateCols[idx])) {
+							const x = form.getFieldValue(qtyPerKitCols[idx])*form.getFieldValue(rateCols[idx]);
+							form.setFieldsValue({
+								[totalMatReqCols[idx]] : x,
+								[totalCostCols[idx]] : Math.pow(x,2),
+							})
+						} else {
+							form.setFieldsValue({
+								[totalMatReqCols[idx]] : 0,
+								[totalCostCols[idx]] : 0,
+							})
+						}
+
+						if ( form.getFieldValue(totalCostCols[idx]) && form.getFieldValue(monthCols[idx]) && form.getFieldValue("yantra_cycle") ) {
+							form.setFieldsValue({
+								[depCostCols[idx]] : ( form.getFieldValue(totalCostCols[idx])/form.getFieldValue(monthCols[idx]) )/( 30*form.getFieldValue("yantra_cycle") ),
+							})
+						} else {
+							form.setFieldsValue({
+								[depCostCols[idx]] : 0,
+							})
+						}
+					}
+				})
+
+			}
+		}
+
+  	},[form,fields,labels])
 
   // const handleFieldsChange = (data) => {
   //   if(data[0]){
