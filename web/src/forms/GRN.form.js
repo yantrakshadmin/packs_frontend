222fd30108a14ae21f061df70df282f1e239ecb9
Rumeet Singh <singh.rumeet5@gmail.com>
2021-01-28 13:36:33 +0530

    filter active in GRN

--- web/src/forms/GRN.form.js
+++ web/src/forms/GRN.form.js
@@ -1,21 +1,24 @@
-import React, { useState } from 'react';
-import { Form, Col, Row, Button, Divider, Spin, message } from 'antd';
-import { GRNFormFields, GRNItemFormFields } from 'common/formFields/GRN.formFields';
-import { useAPI } from 'common/hooks/api';
-import { useHandleForm } from 'hooks/form';
-import { createGRN, editGRN, retrieveGRN } from 'common/api/auth';
-import { PlusOutlined, MinusCircleOutlined } from '@ant-design/icons';
+import React, {useState} from 'react';
+import {Form, Col, Row, Button, Divider, Spin, message} from 'antd';
+import {GRNFormFields, GRNItemFormFields} from 'common/formFields/GRN.formFields';
+import {useAPI} from 'common/hooks/api';
+import {useHandleForm} from 'hooks/form';
+import {createGRN, editGRN, retrieveGRN} from 'common/api/auth';
+import {PlusOutlined, MinusCircleOutlined} from '@ant-design/icons';
 import moment from 'moment';
 import formItem from '../hocs/formItem.hoc';
 
-export const GRNForm = ({ id, onCancel, onDone }) => {
+import _ from 'lodash';
+import {filterActive} from 'common/helpers/mrHelper';
+
+export const GRNForm = ({id, onCancel, onDone}) => {
   const [reqFile, setFile] = useState(null);
 
-  const { data: vendors } = useAPI('/vendors/', {});
-  const { data: warehouses } = useAPI('/warehouse/', {});
-  const { data: products } = useAPI('/products/', {});
+  const {data: vendors} = useAPI('/vendors/', {});
+  const {data: warehouses} = useAPI('/warehouse/', {});
+  const {data: products} = useAPI('/products/', {});
 
-  const { form, submit, loading } = useHandleForm({
+  const {form, submit, loading} = useHandleForm({
     create: createGRN,
     edit: editGRN,
     retrieve: retrieveGRN,
@@ -47,12 +50,12 @@ export const GRNForm = ({ id, onCancel, onDone }) => {
 
   return (
     <Spin spinning={loading}>
-      <Divider orientation='left'>GRN Details</Divider>
-      <Form onFinish={submit} form={form} layout='vertical' hideRequiredMark autoComplete='off'>
-        <Row style={{ justifyContent: 'left' }}>
+      <Divider orientation="left">GRN Details</Divider>
+      <Form onFinish={submit} form={form} layout="vertical" hideRequiredMark autoComplete="off">
+        <Row style={{justifyContent: 'left'}}>
           {GRNFormFields.slice(0, 1).map((item, idx) => (
             <Col span={6}>
-              <div key={idx} className='p-2'>
+              <div key={idx} className="p-2">
                 {formItem({
                   ...item,
                   kwargs: {
@@ -62,7 +65,7 @@ export const GRNForm = ({ id, onCancel, onDone }) => {
                       option.search.toLowerCase().indexOf(input.toLowerCase()) >= 0,
                   },
                   others: {
-                    selectOptions: warehouses || [],
+                    selectOptions: filterActive(_, warehouses) || [],
                     key: 'id',
                     customTitle: 'name',
                     dataKeys: ['address', 'city'],
@@ -74,7 +77,7 @@ export const GRNForm = ({ id, onCancel, onDone }) => {
           ))}
           {GRNFormFields.slice(1, 2).map((item, idx) => (
             <Col span={6}>
-              <div key={idx} className='p-2'>
+              <div key={idx} className="p-2">
                 {formItem({
                   ...item,
                   kwargs: {
@@ -86,7 +89,7 @@ export const GRNForm = ({ id, onCancel, onDone }) => {
                   others: {
                     key: 'id',
                     selectOptions: vendors
-                      ? vendors.filter((vendor) => vendor.type === 'Material')
+                      ? filterActive(_, vendors).filter((vendor) => vendor.type === 'Material')
                       : [],
                     customTitle: 'name',
                     dataKeys: ['street', 'city'],
@@ -97,7 +100,7 @@ export const GRNForm = ({ id, onCancel, onDone }) => {
           ))}
           {GRNFormFields.slice(2, 3).map((item, idx) => (
             <Col span={6}>
-              <div key={idx} className='p-2'>
+              <div key={idx} className="p-2">
                 {formItem({
                   ...item,
                   kwargs: {
@@ -109,7 +112,7 @@ export const GRNForm = ({ id, onCancel, onDone }) => {
                   others: {
                     key: 'id',
                     selectOptions: vendors
-                      ? vendors.filter((vendor) => vendor.type === 'Transporter')
+                      ? filterActive(_, vendors).filter((vendor) => vendor.type === 'Transporter')
                       : [],
                     customTitle: 'name',
                     dataKeys: ['street', 'city'],
@@ -120,53 +123,53 @@ export const GRNForm = ({ id, onCancel, onDone }) => {
           ))}
           {GRNFormFields.slice(3, 4).map((item, idx) => (
             <Col span={6}>
-              <div key={idx} className='p-2'>
+              <div key={idx} className="p-2">
                 {formItem(item)}
               </div>
             </Col>
           ))}
         </Row>
-        <Row style={{ justifyContent: 'left' }}>
+        <Row style={{justifyContent: 'left'}}>
           {GRNFormFields.slice(4, 8).map((item, idx) => (
             <Col span={6}>
-              <div key={idx} className='p-2'>
-                {formItem({ ...item })}
+              <div key={idx} className="p-2">
+                {formItem({...item})}
               </div>
             </Col>
           ))}
         </Row>
-        <Row style={{ justifyContent: 'left' }}>
+        <Row style={{justifyContent: 'left'}}>
           {GRNFormFields.slice(8, 12).map((item, idx) => (
             <Col span={6}>
-              <div key={idx} className='p-2'>
-                {formItem({ ...item })}
+              <div key={idx} className="p-2">
+                {formItem({...item})}
               </div>
             </Col>
           ))}
         </Row>
-        <Row style={{ justifyContent: 'left' }}>
+        <Row style={{justifyContent: 'left'}}>
           {GRNFormFields.slice(12, 13).map((item, idx) => (
             <Col span={6}>
-              <div key={idx} className='p-2'>
-                {formItem({ ...item })}
+              <div key={idx} className="p-2">
+                {formItem({...item})}
               </div>
             </Col>
           ))}
           {GRNFormFields.slice(13, 14).map((item, idx) => (
             <Col span={6}>
-              <div key={idx} className='p-2'>
-                {formItem({ ...item })}
+              <div key={idx} className="p-2">
+                {formItem({...item})}
               </div>
             </Col>
           ))}
           {GRNFormFields.slice(14, 15).map((item, idx) => (
             <Col span={6}>
-              <div key={idx} className='p-2'>
+              <div key={idx} className="p-2">
                 {formItem({
                   ...item,
                   kwargs: {
                     onChange(info) {
-                      const { status } = info.file;
+                      const {status} = info.file;
                       if (status !== 'uploading') {
                         console.log(info.file, info.fileList);
                       }
@@ -183,16 +186,16 @@ export const GRNForm = ({ id, onCancel, onDone }) => {
             </Col>
           ))}
         </Row>
-        <Divider orientation='left'>Product Details</Divider>
-        <Form.List name='items'>
-          {(fields, { add, remove }) => {
+        <Divider orientation="left">Product Details</Divider>
+        <Form.List name="items">
+          {(fields, {add, remove}) => {
             return (
               <div>
                 {fields.map((field, index) => (
-                  <Row align='middle'>
+                  <Row align="middle">
                     {GRNItemFormFields.slice(0, 1).map((item) => (
                       <Col span={5}>
-                        <div className='p-2'>
+                        <div className="p-2">
                           {formItem({
                             ...item,
                             noLabel: index != 0,
@@ -204,7 +207,7 @@ export const GRNForm = ({ id, onCancel, onDone }) => {
                                 option.search.toLowerCase().indexOf(input.toLowerCase()) >= 0,
                             },
                             others: {
-                              selectOptions: products || [],
+                              selectOptions: filterActive(_, products) || [],
                               key: 'id',
                               dataKeys: ['name', 'description', 'category'],
                               customTitle: 'short_code',
@@ -220,7 +223,7 @@ export const GRNForm = ({ id, onCancel, onDone }) => {
                     ))}
                     {GRNItemFormFields.slice(1, 4).map((item) => (
                       <Col span={5}>
-                        <div className='p-2'>
+                        <div className="p-2">
                           {formItem({
                             ...item,
                             noLabel: index != 0,
@@ -236,27 +239,23 @@ export const GRNForm = ({ id, onCancel, onDone }) => {
                       </Col>
                     ))}
                     <Button
-                      type='danger'
-                      style={index != 0 ? { top: '-2vh' } : null}
+                      type="danger"
+                      style={index != 0 ? {top: '-2vh'} : null}
                       onClick={() => {
                         remove(field.name);
                       }}>
-                      <MinusCircleOutlined />
-                      {' '}
-                      Delete
+                      <MinusCircleOutlined /> Delete
                     </Button>
                   </Row>
                 ))}
                 <Form.Item>
                   <Button
-                    type='dashed'
+                    type="dashed"
                     onClick={() => {
                       add();
                     }}
                     block>
-                    <PlusOutlined />
-                    {' '}
-                    Add Item
+                    <PlusOutlined /> Add Item
                   </Button>
                 </Form.Item>
               </div>
@@ -264,11 +263,11 @@ export const GRNForm = ({ id, onCancel, onDone }) => {
           }}
         </Form.List>
         <Row>
-          <Button type='primary' htmlType='submit'>
+          <Button type="primary" htmlType="submit">
             Save
           </Button>
-          <div className='p-2' />
-          <Button type='primary' onClick={onCancel}>
+          <div className="p-2" />
+          <Button type="primary" onClick={onCancel}>
             Cancel
           </Button>
         </Row>
