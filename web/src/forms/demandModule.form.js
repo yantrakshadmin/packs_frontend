2d08415c91486ff440903219db49d32d76c62ce3
Rumeet Singh <singh.rumeet5@gmail.com>
2021-02-01 17:19:47 +0530

    optimizations

--- web/src/forms/demandModule.form.js
+++ web/src/forms/demandModule.form.js
@@ -24,8 +24,6 @@ export const DemandModuleForm = ({id, onCancel, onDone}) => {
 
   const {data: kits} = useControlledSelect(flowId);
 
-  // const [kits, setKits] = useState([]);
-
   const {form, submit, loading} = useHandleForm({
     create: createDm,
     edit: editDm,
@@ -39,36 +37,6 @@ export const DemandModuleForm = ({id, onCancel, onDone}) => {
   });
 
   const [kitQuantities, setKitQuantities] = useState({});
-
-  // useEffect(() => {
-  //   if (id && !loading) {
-  //     const demand_flows = form.getFieldValue('demand_flows');
-  //     if (demand_flows) {
-  //       const DF = demand_flows.map((i, idx) => {
-  //         try {
-  //           const {data: thisFlow} = loadAPI(`/demand-flows/?id=${demand_flows[idx].flow}`);
-  //           const {data: thisKit} = loadAPI(`/demand-kits/?id=${demand_flows[idx].kit}`);
-  //           return {
-  //             ...i,
-  //             part_number: thisKit.part_number,
-  //             // receiver_client_name: thisFlow.receiver_client.name,
-  //             // receiver_client_city: thisFlow.receiver_client.city,
-  //             // flow_days: thisFlow.flow_days,
-  //             kit_type: thisKit.kit_type,
-  //             kit_id: thisKit.kit_name,
-  //             components_per_kit: thisKit.components_per_kit,
-  //           };
-  //         } catch (err) {
-  //           console.log(err);
-  //           return null;
-  //         }
-  //       });
-
-  //       console.log(DF);
-  //       form.setFieldsValue({demand_flows: DF});
-  //     }
-  //   }
-  // }, [id, loading]);
 
   useEffect(() => {
     if (id && !loading) {
@@ -103,78 +71,27 @@ export const DemandModuleForm = ({id, onCancel, onDone}) => {
 
   const handleFieldsChange = useCallback(
     (data) => {
-      if (data[0]) {
-        if (data[0].name) {
-          if (data[0].name[0] === 'demand_flows') {
-            const fields = form.getFieldsValue();
-
-            if ('demand_flows' in fields) {
-              const fieldKey = data[0].name[1];
-              const flowsX = fields['demand_flows'];
-              if (fieldKey in flowsX) {
-                if ('flow' in flowsX[fieldKey]) {
-                  const thisFlow = _.find(flows, (o) => o.id === flowsX[fieldKey].flow);
-                  if ('kit' in flowsX[fieldKey]) {
-                    const thisKit = _.find(kits, (o) => o.id === flowsX[fieldKey].kit);
-                    if (thisFlow && thisKit) {
-                      Object.assign(flowsX[fieldKey], {
-                        part_number: thisKit.part_number,
-                        receiver_client_name: thisFlow.receiver_client.name,
-                        receiver_client_city: thisFlow.receiver_client.city,
-                        flow_days: thisFlow.flow_days,
-                        kit_type: thisKit.kit_type,
-                        kit_id: thisKit.kit_name,
-                        components_per_kit: thisKit.components_per_kit,
-                      });
-                    }
-                  }
-                  form.setFieldsValue({demand_flows: flowsX});
-                  console.log(flowsX);
-                }
-              }
-            }
-          }
+      try {
+        if (data[0].name[0] === 'demand_flows') {
+          const fieldKey = data[0].name[1];
+          const flowsX = form.getFieldValue('demand_flows');
+          const thisFlow = _.find(flows, (o) => o.id === flowsX[fieldKey].flow);
+          const thisKit = _.find(kits, (o) => o.id === flowsX[fieldKey].kit);
+          Object.assign(flowsX[fieldKey], {
+            part_number: thisKit.part_number,
+            receiver_client_name: thisFlow.receiver_client.name,
+            receiver_client_city: thisFlow.receiver_client.city,
+            flow_days: thisFlow.flow_days,
+            kit_type: thisKit.kit_type,
+            kit_id: thisKit.kit_name,
+            components_per_kit: thisKit.components_per_kit,
+          });
+          form.setFieldsValue({demand_flows: flowsX});
         }
-      }
+      } catch (err) {}
     },
     [kits, flows, form],
   );
-  // const renderDFTable = useCallback(
-  //   (fieldKey) => {
-  //     try {
-  //       const flowsX = form.getFieldsValue('demand_flows');
-  //       const thisFlow = _.find(flows, (o) => o.id === flowsX[fieldKey].flow);
-  //       const thisKit = _.find(kits, (o) => o.id === flowsX[fieldKey].kit);
-  //       const dataSource = [
-  //         {
-  //           part_number: thisKit.part_number,
-  //           receiver_client_name: thisFlow.receiver_client.name,
-  //           receiver_client_city: thisFlow.receiver_client.city,
-  //           flow_days: thisFlow.flow_days,
-  //           kit_type: thisKit.kit_type,
-  //           kit_id: thisKit.kit_name,
-  //           components_per_kit: thisKit.components_per_kit,
-  //         },
-  //       ];
-  //       return <Table columns={demandFlowsCols} dataSource={dataSource} size="small" />;
-  //     } catch (err) {
-  //       console.log(err);
-  //       const dataSource = [
-  //         {
-  //           part_number: '-',
-  //           receiver_client_name: '-',
-  //           receiver_client_city: '-',
-  //           flow_days: '-',
-  //           kit_type: '-',
-  //           kit_id: '-',
-  //           components_per_kit: '-',
-  //         },
-  //       ];
-  //       return <Table columns={demandFlowsCols} dataSource={dataSource} size="small" />;
-  //     }
-  //   },
-  //   [flowId, kits, flows, form],
-  // );
 
   return (
     <Spin spinning={loading}>
@@ -339,3 +256,108 @@ export const DemandModuleForm = ({id, onCancel, onDone}) => {
     </Spin>
   );
 };
+
+// useEffect(() => {
+//   if (id && !loading) {
+//     const demand_flows = form.getFieldValue('demand_flows');
+//     if (demand_flows) {
+//       const DF = demand_flows.map((i, idx) => {
+//         try {
+//           const {data: thisFlow} = loadAPI(`/demand-flows/?id=${demand_flows[idx].flow}`);
+//           const {data: thisKit} = loadAPI(`/demand-kits/?id=${demand_flows[idx].kit}`);
+//           return {
+//             ...i,
+//             part_number: thisKit.part_number,
+//             // receiver_client_name: thisFlow.receiver_client.name,
+//             // receiver_client_city: thisFlow.receiver_client.city,
+//             // flow_days: thisFlow.flow_days,
+//             kit_type: thisKit.kit_type,
+//             kit_id: thisKit.kit_name,
+//             components_per_kit: thisKit.components_per_kit,
+//           };
+//         } catch (err) {
+//           console.log(err);
+//           return null;
+//         }
+//       });
+
+//       console.log(DF);
+//       form.setFieldsValue({demand_flows: DF});
+//     }
+//   }
+// }, [id, loading]);
+
+// const handleFieldsChange = useCallback(
+//   (data) => {
+//     if (data[0]) {
+//       if (data[0].name) {
+//         if (data[0].name[0] === 'demand_flows') {
+//           const fields = form.getFieldsValue();
+
+//           if ('demand_flows' in fields) {
+//             const fieldKey = data[0].name[1];
+//             const flowsX = fields['demand_flows'];
+//             if (fieldKey in flowsX) {
+//               if ('flow' in flowsX[fieldKey]) {
+//                 const thisFlow = _.find(flows, (o) => o.id === flowsX[fieldKey].flow);
+//                 if ('kit' in flowsX[fieldKey]) {
+//                   const thisKit = _.find(kits, (o) => o.id === flowsX[fieldKey].kit);
+//                   if (thisFlow && thisKit) {
+//                     Object.assign(flowsX[fieldKey], {
+//                       part_number: thisKit.part_number,
+//                       receiver_client_name: thisFlow.receiver_client.name,
+//                       receiver_client_city: thisFlow.receiver_client.city,
+//                       flow_days: thisFlow.flow_days,
+//                       kit_type: thisKit.kit_type,
+//                       kit_id: thisKit.kit_name,
+//                       components_per_kit: thisKit.components_per_kit,
+//                     });
+//                   }
+//                 }
+//                 form.setFieldsValue({demand_flows: flowsX});
+//                 console.log(flowsX);
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   },
+//   [kits, flows, form],
+// );
+// const renderDFTable = useCallback(
+//   (fieldKey) => {
+//     try {
+//       const flowsX = form.getFieldsValue('demand_flows');
+//       const thisFlow = _.find(flows, (o) => o.id === flowsX[fieldKey].flow);
+//       const thisKit = _.find(kits, (o) => o.id === flowsX[fieldKey].kit);
+//       const dataSource = [
+//         {
+//           part_number: thisKit.part_number,
+//           receiver_client_name: thisFlow.receiver_client.name,
+//           receiver_client_city: thisFlow.receiver_client.city,
+//           flow_days: thisFlow.flow_days,
+//           kit_type: thisKit.kit_type,
+//           kit_id: thisKit.kit_name,
+//           components_per_kit: thisKit.components_per_kit,
+//         },
+//       ];
+//       return <Table columns={demandFlowsCols} dataSource={dataSource} size="small" />;
+//     } catch (err) {
+//       console.log(err);
+//       const dataSource = [
+//         {
+//           part_number: '-',
+//           receiver_client_name: '-',
+//           receiver_client_city: '-',
+//           flow_days: '-',
+//           kit_type: '-',
+//           kit_id: '-',
+//           components_per_kit: '-',
+//         },
+//       ];
+//       return <Table columns={demandFlowsCols} dataSource={dataSource} size="small" />;
+//     }
+//   },
+//   [flowId, kits, flows, form],
+// );
