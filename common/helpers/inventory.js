6b2295fdd99bbc802cbab916a7615af8c43f0e07
Rumeet Singh <singh.rumeet5@gmail.com>
2021-03-09 16:39:01 +0530

    product info added in sender client inv

--- common/helpers/inventory.js
+++ common/helpers/inventory.js
@@ -1,17 +1,26 @@
-export const getReformattedData=(obj={})=>(Object.keys(obj).map((key=>({
-  short_code:key,
-  quantity:obj[key][0],
-  description:obj[key][1]
-}))))
+export const getReformattedData = (obj = {}) =>
+  Object.keys(obj).map((key) => ({
+    short_code: key,
+    quantity: obj[key][0],
+    description: obj[key][1],
+  }));
 
-export const getReformattedAllotmentData = (obj={}) => (Object.keys(obj).map((key => ({
-  short_code : key,
-  quantity : obj[key][0],
-  info : obj[key][1],
-}))))
+export const getReformattedAllotmentData = (obj = {}) =>
+  Object.keys(obj).map((key) => ({
+    short_code: key,
+    quantity: obj[key][0],
+    info: obj[key][1],
+  }));
 
-export const getReformattedReturnData = (obj={}) => (Object.keys(obj).map((key => ({
-  	short_code : key,
-  quantity : obj[key][0],
-  info : obj[key][1],
-}))))
+export const getReformattedReturnData = (obj = {}) =>
+  Object.keys(obj).map((key) => ({
+    short_code: key,
+    quantity: obj[key][0],
+    info: obj[key][1],
+  }));
+
+export const getReformattedProductInfoData = (obj = {}) =>
+  Object.keys(obj).map((key) => ({
+    short_code: key,
+    quantity: obj[key],
+  }));
