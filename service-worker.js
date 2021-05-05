bfa1d722b5ffea427589aec16822a580c8daa8ec
Rumeet Singh <singh.rumeet5@gmail.com>
2021-05-05 13:58:51 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0b78d51b27c099fb9bba536707e23c12.js"
+  "/precache-manifest.bd318208d5b699318b607907c3a6737d.js"
 );
 
 self.addEventListener('message', (event) => {
--- static/js/91.89cfd647.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpweb=this.webpackJsonpweb||[]).push([[91],{830:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a),s=n(36);t.default=function(){return r.a.createElement(s.b,{to:"/client/material-request/"})}}}]);
-//# sourceMappingURL=91.89cfd647.chunk.js.map
