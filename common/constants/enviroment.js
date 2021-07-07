46b6960e8b02436075252d2c6f1f10893808d023
asherM16 <ashertoufeeq@Ashers-MacBook-Air.local>
2021-07-07 18:13:58 +0530

    feat(material-request): create-allotment

--- common/constants/enviroment.js
+++ common/constants/enviroment.js
@@ -1,3 +1,3 @@
 // export const DEFAULT_BASE_URL = 'https://packsdev.ap-south-1.elasticbeanstalk.com';
 export const DEFAULT_BASE_URL = 'https://packsb.yantraksh.com/';
-//export const DEFAULT_BASE_URL = 'https://4eec21ccdd28.in.ngrok.io';
+// export const DEFAULT_BASE_URL = 'http://137a1345f49f.in.ngrok.io/';
